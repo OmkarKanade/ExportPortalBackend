31453f8f12c74648b1378ba6730da1c8a4c1b159
OmkarKanade <omkarkanade8888@gmail.com>
2024-02-26 16:27:31 +0530

    added Sid in JWT Token

--- ExportPortal.API/Controllers/LoginController.cs
+++ ExportPortal.API/Controllers/LoginController.cs
@@ -64,6 +64,7 @@ namespace ExportPortal.API.Controllers
             var claims = new List<Claim>();
 
             claims.Add(new Claim(ClaimTypes.Email, user.Email));
+            claims.Add(new Claim(ClaimTypes.Sid, user.Id));
 
             foreach (var role in roles)
             {
